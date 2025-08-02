3fb9068f1341eac044238a59e4b57408b834eac1
karthi <karthijayasangar6564@gmail.com>
2025-08-02 16:33:08 +0530

    fix loading

--- src/pages/SearchMovie.jsx
+++ src/pages/SearchMovie.jsx
@@ -38,66 +38,66 @@ const SearchMovie = () => {
 
         }
         return <div className='p-1 mt-4'>
-        <p className='p-1 rounded-xl shadow-2xl text-lg text-white border font-bold italic ml-30 mr-30 sm:w-[20%] text-center sm:ml-[40%] mb-1 bg-lime-300'>Search Results</p>
-        <div className="flex flex-row gap-3 flex-wrap justify-around p-2 mb-10"><Rendermovie /></div>
-        <div className='flex justify-center gap-2 mb-5 mt-[-20px]'>
-            <button className='p-1 w-8 rounded-full shadow border-none shadow-amber-400'
-                onClick={() => {
-                    if (number > 0) {
-                        setnumber(number - 1)
-                    }
-                }}>-</button>
-            <button className=' bg-yellow-300 text-white font-bold p-1 w-8 rounded-full shadow border-none '>{number}</button>
-            <button onClick={() => {
-                if (number < (Length - 1))
-                    setnumber(number + 1)
-            }} className='p-1 rounded-full shadow border-none shadow-amber-300 w-8 '>+</button>
-        </div>
-
-        <Outlet />
-    </div>
-
-
-    }else {
-
-    const Length = data.length;
-    const movies = data[number].Search;
-    const Rendermovie = () => {
-
-        return movies.map((movie, index) => {
-            return <div key={index} onClick={() => {
-                localStorage.setItem("movie", JSON.stringify(movie));
-                navigate('/Detailed');
-            }} className="w-55 h-80 mb-8" >
-                <img src={movie.Poster} onError={(e) => {
-                    e.target.onError = null;
-                    e.target.src = '/emty2.jpg';
-                }} alt=' No Poster Available....' className=" size-[100%] rounded-lg" />
-                <p className="text-xs font-light mt-1">{movie.Title}</p>
+            <p className='p-1 rounded-xl shadow-2xl text-lg text-white border font-bold italic ml-30 mr-30 sm:w-[20%] text-center sm:ml-[40%] mb-1 bg-lime-300'>Search Results</p>
+            <div className="flex flex-row gap-3 flex-wrap justify-around p-2 mb-10"><Rendermovie /></div>
+            <div className='flex justify-center gap-2 mb-5 mt-[-20px]'>
+                <button className='p-1 w-8 rounded-full shadow border-none shadow-amber-400'
+                    onClick={() => {
+                        if (number > 0) {
+                            setnumber(number - 1)
+                        }
+                    }}>-</button>
+                <button className=' bg-yellow-300 text-white font-bold p-1 w-8 rounded-full shadow border-none '>{number}</button>
+                <button onClick={() => {
+                    if (number < (Length - 1))
+                        setnumber(number + 1)
+                }} className='p-1 rounded-full shadow border-none shadow-amber-300 w-8 '>+</button>
             </div>
 
-        });
-
-    }
-    return <div className='p-1 mt-4'>
-        <p className='p-1 rounded-xl shadow-2xl text-lg text-white border font-bold italic ml-30 mr-30 sm:w-[20%] text-center sm:ml-[40%] mb-1 bg-lime-300'>Search Results</p>
-        <div className="flex flex-row gap-3 flex-wrap justify-around p-2 mb-10"><Rendermovie /></div>
-        <div className='flex justify-center gap-2 mb-5 mt-[-20px]'>
-            <button className='p-1 w-8 rounded-full shadow border-none shadow-amber-400'
-                onClick={() => {
-                    if (number > 0) {
-                        setnumber(number - 1)
-                    }
-                }}>-</button>
-            <button className=' bg-yellow-300 text-white font-bold p-1 w-8 rounded-full shadow border-none '>{number}</button>
-            <button onClick={() => {
-                if (number < (Length - 1))
-                    setnumber(number + 1)
-            }} className='p-1 rounded-full shadow border-none shadow-amber-300 w-8 '>+</button>
+            <Outlet />
         </div>
 
-        <Outlet />
-    </div>
+
+    } else {
+
+        const Length = data.length;
+        const movies = data[number].Search;
+        const Rendermovie = () => {
+
+            return movies.map((movie, index) => {
+                return <div key={index} onClick={() => {
+                    localStorage.setItem("movie", JSON.stringify(movie));
+                    navigate('/Detailed');
+                }} className="w-55 h-80 mb-8" >
+                    <img src={movie.Poster} onError={(e) => {
+                        e.target.onError = null;
+                        e.target.src = '/emty2.jpg';
+                    }} alt=' No Poster Available....' className=" size-[100%] rounded-lg" />
+                    <p className="text-xs font-light mt-1">{movie.Title}</p>
+                </div>
+
+            });
+
+        }
+        return <div className='p-1 mt-4'>
+            <p className='p-1 rounded-xl shadow-2xl text-lg text-white border font-bold italic ml-30 mr-30 sm:w-[20%] text-center sm:ml-[40%] mb-1 bg-lime-300'>Search Results</p>
+            <div className="flex flex-row gap-3 flex-wrap justify-around p-2 mb-10"><Rendermovie /></div>
+            <div className='flex justify-center gap-2 mb-5 mt-[-20px]'>
+                <button className='p-1 w-8 rounded-full shadow border-none shadow-amber-400'
+                    onClick={() => {
+                        if (number > 0) {
+                            setnumber(number - 1)
+                        }
+                    }}>-</button>
+                <button className=' bg-yellow-300 text-white font-bold p-1 w-8 rounded-full shadow border-none '>{number}</button>
+                <button onClick={() => {
+                    if (number < (Length - 1))
+                        setnumber(number + 1)
+                }} className='p-1 rounded-full shadow border-none shadow-amber-300 w-8 '>+</button>
+            </div>
+
+            <Outlet />
+        </div>
     }
 }
 
